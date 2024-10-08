dcf637f6d847e4f395f7d7c1144a6afd31ff09e6
saitejamandadi99 <saitejamandadi99>
2024-10-08 13:47:42 +0000

    initial code

---
+++ src/components/GithubPopularRepos/index.js
@@ -0,0 +1,26 @@
+import {Component} from 'react'
+
+import LanguageFilterItem from '../LanguageFilterItem'
+
+import RepositoryItem from '../RepositoryItem'
+
+import './index.css'
+
+const languageFiltersData = [
+  {id: 'ALL', language: 'All'},
+  {id: 'JAVASCRIPT', language: 'Javascript'},
+  {id: 'RUBY', language: 'Ruby'},
+  {id: 'JAVA', language: 'Java'},
+  {id: 'CSS', language: 'CSS'},
+]
+
+// Write your code here
+class GithubPopularRepos extends Component {
+  render(){
+    return(
+      <div className='appContainer'></div>
+    )
+  }
+}
+
+export default GithubPopularRepos
---
+++ src/components/LanguageFilterItem/index.js
@@ -0,0 +1,4 @@
+// Write your code here
+const LanguageFilterItem = props => {}
+
+export default LanguageFilterItem
